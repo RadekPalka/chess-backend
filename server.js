7321f542763431415a8a50170ec2aaf5906f54df
Radosław Pałka <radekpalka@gmail.com>
2025-03-23 18:03:24 +0100

    feat: add join room logic

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 
-const rooms =[{name: "room1", players: []}, {name: 'room2', players: []}]
+const rooms = [
+	{ name: 'room1', players: new Set() },
+	{ name: 'room2', players: new Set() },
+];
 const io = new Server(3000, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"]
-  }
+	cors: {
+		origin: 'http://localhost:5173',
+		methods: ['GET', 'POST'],
+	},
 });
 
 const broadcastRooms = () => {
-  io.emit("updateRooms", rooms);
+	io.emit(
+		'updateRooms',
+		rooms.map((room) => ({ name: room.name, numberOfPlayers: room.players.size }))
+	);
 };
 
-io.on('connection', socket=>{
-  socket.emit("updateRooms", rooms.map(room=> ({name: room.name, numberOfPlayers: room.players.length})));
-  socket.on("joinRoom", (roomName)=>{
-    console.log(roomName, socket.id)
-  })
-})
+io.on('connection', (socket) => {
+	console.log('Connection');
+	broadcastRooms();
+	socket.on('joinRoom', (roomName, userName) => {
+		console.log(roomName, socket.id, userName);
+		const room = rooms.find((room) => room.name === roomName);
+		room.players.add(userName);
+		broadcastRooms();
+		console.log(room);
+	});
+});
